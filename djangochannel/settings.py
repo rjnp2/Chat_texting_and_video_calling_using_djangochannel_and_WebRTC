e49a9968bd92dc16c121b7f941a046fc5a3c4eac
Rajan Pandey <58425689+rjnp2@users.noreply.github.com>
2022-03-24 11:35:20 +0545

    Update settings.py

--- djangochannel/settings.py
+++ djangochannel/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@64q9p%n-si)8_$=d0sgzt8smi@puf-6rc8pstj)z85#@dt97y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
